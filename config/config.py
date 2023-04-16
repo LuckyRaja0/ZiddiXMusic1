91068e60fba8b5e265f71a590f17698d3af28b5a
Lucky Raja <129265896+LuckyRaja0@users.noreply.github.com>
2023-04-16 14:21:09 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,9 +43,9 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
 
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "")
+    "SUPPORT_CHANNEL", "https://t.me/QuizStudyUPSC")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "")
+    "SUPPORT_GROUP", "https://t.me/Doston_Ki_Masti")
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
