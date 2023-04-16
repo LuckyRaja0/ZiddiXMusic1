f4ce94e62d65f8e8d39ca1f65326db6a45607b4a
Lucky Raja <129265896+LuckyRaja0@users.noreply.github.com>
2023-04-16 13:58:52 +0530

    Update start.py

--- VenomX/plugins/Robot/start.py
+++ VenomX/plugins/Robot/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CcRSZLgACLgFkOtPGWlXg58oVVr1DRRYydgHpsAAC7AQAAlbB2FedaSmY0RA9uB4E")
+            await message.reply_sticker("CAACAgUAAx0CcRSZLgACL4BkO7F6gMkHtf8L7On7DtEemBH3vAACygYAAkopyFeo2MNmvQxxlR4E")
             return await message.reply_photo("https://te.legra.ph/file/487e890c990f5c9b1e1c5.jpg"
                        photo=config.START_IMG_URL,
                        caption=_["help_1"], reply_markup=keyboard
